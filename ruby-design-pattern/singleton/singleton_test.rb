7444f018afd17b711e92c21aafc3f6b9b1995e5c
kimromi <hiromi19860101@gmail.com>
2017-06-23 00:01:06 +0900

    Add test

--- ruby-design-pattern/singleton/singleton_test.rb
+++ ruby-design-pattern/singleton/singleton_test.rb
@@ -7,4 +7,10 @@ class TestSingleton < MiniTest::Test
     object2 = Singleton.get_instance
     assert_equal object1.object_id, object2.object_id
   end
+
+  def test_uncallable_new
+    assert_raises NoMethodError do
+      Singleton.new
+    end
+  end
 end
