515da674fac2334e9c8281fff815b5e31493a0f0
kimromi <kimromi4@gmail.com>
2017-07-20 01:43:48 +0900

    add test

--- ruby-design-pattern/abstract_factory/abstract_factory_test.rb
+++ ruby-design-pattern/abstract_factory/abstract_factory_test.rb
@@ -11,4 +11,14 @@ class TestAbstractFactory < MiniTest::Test
       assert_equal "藻 植物 #{i} は成長中です", plant.grow
     end
   end
+
+  def test_duck_and_water_lily
+    factory = DuckAndWaterLilyFactory.new(5, 3)
+    factory.get_animals.each_with_index do |animal, i|
+      assert_equal "アヒル 動物 #{i} は食事中です", animal.eat
+    end
+    factory.get_plants.each_with_index do |plant, i|
+      assert_equal "スイレン 植物 #{i} は成長中です", plant.grow
+    end
+  end
 end
