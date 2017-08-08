35521c094dba4d3c1294ed4cdb191f479273fada
kimromi <kimromi4@gmail.com>
2017-08-08 16:55:34 +0900

    setting role

--- rails-admin/admin/app/assets/javascripts/admin/application.js
+++ rails-admin/admin/app/assets/javascripts/admin/application.js
@@ -1,4 +1,5 @@
 //= require jquery
+//= require jquery_ujs
 //= require vendor/bootstrap.min
 //= require vendor/admin-lte.min
 //= require_tree .
--- rails-admin/admin/app/controllers/admin/employees_controller.rb
+++ rails-admin/admin/app/controllers/admin/employees_controller.rb
@@ -8,10 +8,29 @@ module Admin
       @employees = Employee.all
     end
 
+    def show
+      @employee = Employee.find(params[:id])
+    end
+
+    def update
+      @employee = Employee.find(params[:id])
+      if @employee.update(update_params)
+        redirect_to employees_path, notice: 'Employee was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
     def destroy
       Employee.find(params[:id]).destroy
       flash[:success] = "Employee Deleted."
       redirect_to employees_url
+    end
+
+    private
+
+    def update_params
+      params.require(:employee).permit(:role)
     end
   end
 end
