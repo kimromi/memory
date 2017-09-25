fd32fe3166e1ed59b5a82125ad885089235f820b
kimromi <kimromi4@gmail.com>
2017-09-26 02:37:12 +0900

    chef-handsonはリポジトリにしました

--- chef-handson/.chef/knife.rb
+++
@@ -1,8 +0,0 @@
-local_mode true
-cookbook_path ["cookbooks", "vendor/cookbooks"]
-role_path     "roles"
-data_bag_path "data_bags"
-#encrypted_data_bag_secret "secret_data_bag_key"
-
-knife[:use_sudo] = true
-knife[:ssh_user] = 'vagrant'
--- chef-handson/cookbooks/rails/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default['rails']['app_directory'] = '/var/www/rails'
-default['rails']['fqdn'] = 'chef1.example'
